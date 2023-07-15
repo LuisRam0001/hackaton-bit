c6d31d4917e39c7ff4ad37e687fc5c016115fdf0
luis ramirez <luis.ramirez0001@outlook.com>
2023-07-14 21:34:33 -0500

    [+] se intenta hacer el punto 5 pero hay error de capa 8 se evidencia que hay que seguir practicando

--- punto5.js
+++ punto5.js
@@ -1,43 +1,48 @@
  //escribe una funcion que reciba un numero como parametro y determine si es un numero primo .Devuelve true si es primo y false si no lo es 
-//NO se puede usar operaciones del lenguaje que realicen esta tarea directamente
-
-
-//let numero = Number(prompt("Ingrese un número "));
-//
-//function esPrimo(numero) {
-//   if (numero < 1) {
-//     return false;
-//   }
-//   for (let i = 1; i <= numero; i++) {
-//     if (numero % i === numero) {
-//       return false;
-//     }
-//   }
-//   return true;
-// }
-// 
-// console.log(esPrimo(numero));
-// 
-
-
-
-let numero = Number(prompt('Ingrese un numero'));
-
-for (let i = 2; i <= numero; i++) {
-  let esPrimo = true;
-
-  for (let j = 2; j < i; j++) {
-    if (i % j === 0) {
-      esPrimo = false;
-      
-    };
+ //NO se puede usar operaciones del lenguaje que realicen esta tarea directamente
+ 
+ 
+ let numeroPrimo = Number(prompt('Ingrese un numero'));
+ 
+ function esPrimo(numero) {
+    if (numero < 2) {
+      return false;
+    }
+  
+    for (let i = 2; i <= numero / 2; i++) {
+      if (numero % i === 0) {
+        return false;
+      }
+    }
+  
+    return true;
   }
+  
+  console.log(numeroPrimo); 
+  
+  
 
-  if (esPrimo) {
-    console.log(i);
 
-  }
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
